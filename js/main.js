10dbf6b1f21e45b2692b3b38bfd77e7ee4996adb
Sujay Garlanka <sujay.garlanka@gmail.com>
2021-03-18 21:18:06 -0400

    fix scrolling and make more mobile friendly

--- js/main.js
+++ js/main.js
@@ -9,8 +9,45 @@ window.onscroll = function () {
   }
 };
 
-window.setTimeout(function () {
-  document.querySelector(window.location.hash).scrollIntoView({
-    behavior: 'smooth'
-});
-}, 1000);
+window.onload = (event) => {
+  window.setTimeout(function () {
+    setScrollForLinks();
+    scrollToSection();
+  }, 700);
+};
+
+function scrollToSection() {
+  $("html, body").animate(
+    {
+      scrollTop: $(window.location.hash).offset().top,
+    },
+    500
+  );
+}
+
+function setScrollForLinks() {
+  // Add smooth scrolling to all links
+  $("a").on("click", function (event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        500,
+        function () {
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        }
+      );
+    } // End if
+  });
+}
