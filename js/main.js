58774a7b05a8d331e48edd9dd8604788ad73e00c
Sujay Garlanka <sujay.garlanka@gmail.com>
2021-07-04 15:54:50 -0400

    add new font and make it more minimalist

--- js/main.js
+++ js/main.js
@@ -3,9 +3,11 @@ $( document ).ready(function() {
 });
 
 function goToTab() {
-  var someTabTriggerEl = document.querySelector(window.location.hash);
-  var tab = new bootstrap.Tab(someTabTriggerEl)
-  tab.show()
+  // var someTabTriggerEl = document.querySelector(window.location.hash);
+  var tabs = $( ".drink" );
+  tabs[0].click();
+  tabs[1].click();
+  var tab = new bootstrap.Tab(someTabTriggerEl);
+  console.log(tab)
+  tab.show();
 }
-
-feather.replace()
