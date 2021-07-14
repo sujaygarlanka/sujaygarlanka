63ac2c38f42052ab472786d272bde878dd190be2
Sujay Garlanka <sujay.garlanka@gmail.com>
2021-07-13 20:09:31 -0400

    home page redesign,mobile friendly and resume rendering wip

--- js/main.js
+++ js/main.js
@@ -3,11 +3,7 @@ $( document ).ready(function() {
 });
 
 function goToTab() {
-  // var someTabTriggerEl = document.querySelector(window.location.hash);
-  var tabs = $( ".drink" );
-  tabs[0].click();
-  tabs[1].click();
+  var someTabTriggerEl = document.querySelector(window.location.hash);
   var tab = new bootstrap.Tab(someTabTriggerEl);
-  console.log(tab)
   tab.show();
 }
