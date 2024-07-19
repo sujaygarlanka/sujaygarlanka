e54cc3a9e38a2867ccde499e271d5b129fad187b
sujaygarlanka <sujay.garlanka@gmail.com>
2024-07-18 17:29:18 -0700

    update train

--- js/train.js
+++ js/train.js
@@ -11,3 +11,90 @@ const ys = tf.tensor2d([-3, -1, 1, 3, 5, 7], [6, 1]);
 
 // Train the model using the data.
 await model.fit(xs, ys, {epochs: 250});
+
+
+
+class DQNAgent {
+    constructor(stateSize, actionSize, model, nEpisodes) {
+        this.stateSize = stateSize;
+        this.actionSize = actionSize;
+        this.memory = [];
+        this.memoryMaxLength = 2000;
+
+        this.gamma = 0.95;  // discount rate
+        this.epsilon = 1.0;
+        this.epsilonMin = 0.01;
+        this.epsilonDecay = Math.pow(this.epsilonMin / this.epsilon, 1 / nEpisodes);
+
+        this.learningRate = 0.001;
+        this.model = model;
+        this.lossFn = (output, targetOutput) => {
+            // Mean Squared Error (MSE) loss function
+            return output.reduce((acc, curr, idx) => acc + Math.pow(curr - targetOutput[idx], 2), 0) / output.length;
+        };
+    }
+
+    remember(state, action, reward, nextState, done) {
+        if (this.memory.length >= this.memoryMaxLength) {
+            this.memory.shift();
+        }
+        this.memory.push({ state, action, reward, nextState, done });
+    }
+
+    act(state, evalMode = false) {
+        if (!evalMode && Math.random() <= this.epsilon) {
+            return Math.random() < 0.75 ? 0 : 1;
+        }
+        const actValues = this.model.predict([state]);
+        return actValues.indexOf(Math.max(...actValues));
+    }
+
+    replay(batchSize) {
+        const minibatch = [];
+        for (let i = 0; i < batchSize; i++) {
+            const idx = Math.floor(Math.random() * this.memory.length);
+            minibatch.push(this.memory[idx]);
+        }
+
+        const states = minibatch.map(m => m.state);
+        const actions = minibatch.map(m => m.action);
+        const rewards = minibatch.map(m => m.reward);
+        const nextStates = minibatch.map(m => m.nextState);
+        const dones = minibatch.map(m => m.done);
+
+        const outputs = this.model.predict(nextStates);
+        const targets = rewards.map((reward, i) => {
+            return dones[i] ? reward : reward + this.gamma * Math.max(...outputs[i]);
+        });
+
+        const output = this.model.predict(states);
+
+        const targetOutput = [...output];
+        for (let i = 0; i < actions.length; i++) {
+            targetOutput[i][actions[i]] = targets[i];
+        }
+
+        const loss = this.lossFn(output, targetOutput);
+        this.model.backward(loss);
+
+        // Update weights (assuming model has methods to get and set parameters)
+        const { params, grads } = this.model.getParameters();
+        const updatedParams = params.map((param, i) => param - this.learningRate * grads[i]);
+        this.model.setParameters(updatedParams);
+
+        if (this.epsilon > this.epsilonMin) {
+            this.epsilon *= this.epsilonDecay;
+        }
+    }
+}
+
+// Example usage
+// Assuming you have a model object with methods `predict`, `backward`, `getParameters`, and `setParameters`
+const model = {
+    predict: (state) => [/* Your prediction logic here */],
+    backward: (loss) => { /* Your backpropagation logic here */ },
+    getParameters: () => ({ params: [/* Your parameters here */], grads: [/* Your gradients here */] }),
+    setParameters: (params) => { /* Your logic to set parameters here */ }
+};
+
+const agent = new DQNAgent(4, 2, model, 1000);
